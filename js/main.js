9eeba24f488bf346ff52bf674675215dfcf017a1
John Rae <john.rae23@gmail.com>
2016-03-21 16:44:04 -0400

    x and y axis rotate correctly; Minor performance fixes

--- js/main.js
+++ js/main.js
@@ -1,35 +1,73 @@
 import $ from 'jquery';
 
-let $listItems = $('.spinner-container>ul').children();
-let $spinner = $('.spinner-container');
+// variable declarations
+let $listItems = $('.turntable>ul').children();
+let $turntable = $('.turntable');
 let sections = [];
+let axisValue = $('.turntable').data('axis');
 
-let divideContainer = function(images){
+// splits container based on 
+// amount of li's in turntable
+function divideContainer(images){
+
+  var initialLength,  
+      dividend = images.length;
+
+  if( axisValue === 'y') {
+    initialLength = $turntable.height();
+  } else {
+    initialLength = $turntable.width();
+  }
+
+  let sectionLength = initialLength/dividend;
   
-  let initialWidth = $spinner.width();
-  let divider      = images.length;
-  let sectionWidth = initialWidth/divider;
-  
+  // creates array of value pairs with min and max ranges
   for (var j = 0; j < images.length; j++){
     sections[j] = {   
-      min: sectionWidth * j,
-      max: sectionWidth + (sectionWidth * j),
+      min: sectionLength * j,
+      max: sectionLength + (sectionLength * j),
       index: j
     };
   }
-};
+}
 
-divideContainer($listItems);
+//loads images one at a time on page load
+function appendImages(callback) {
+  $listItems.each(function(){
+    $(this).append('<img src="' + 
+      $(this).data("imgSrc") +
+      '">');
+  });
+}
 
-$spinner.on("mousemove", function(e) {
+appendImages();
+
+// divides container once image is loaded
+$( "li:first-child>img", $turntable ).load(function() {
+  $(this).parent().addClass('active');
+  divideContainer($listItems);
+});
+
+// finds mouse position and appends body
+// based on location
+$turntable.on("mousemove", function(e) {
 
   let offset = $(this).offset();
-  let relX = e.pageX - offset.left;
+  let position;
 
+  if( axisValue === 'y' ) {
+    position = e.pageY - offset.top;
+  } else {
+    position = e.pageX - offset.left;
+  }
+
+  // loop through array and find correct range pair
   $.each(sections, function(){
-    if(relX >= this.min && relX <= this.max) {
+    if(position >= this.min && position <= this.max) {
       $listItems.removeClass('active');
       $listItems.eq(this.index).addClass("active");
     }
   });
+  
 });
+ 

759c38bde700fb80a70362ab1812248b097cd047
John Rae <john.rae23@gmail.com>
2016-03-21 11:34:51 -0400

    spinner is working correctly

--- js/main.js
+++ js/main.js
@@ -1,70 +1,35 @@
 import $ from 'jquery';
-import _ from 'underscore';
 
-
-let listItems = $('.spinner-container>ul').children();
+let $listItems = $('.spinner-container>ul').children();
 let $spinner = $('.spinner-container');
+let sections = [];
 
-let initialWidth = $('.spinner-container').width();
-let divider      = listItems.length;
-let sectionWidth = initialWidth/divider;
-
-function divideContainer(images){
-
-  let sections = [];
+let divideContainer = function(images){
   
-  // let i = 0  
-  // $(".spinner-container li").each(function( index ) {
-  //   $(this).data("max")
-  //   i += sectionWidth.to_i
-  // })
+  let initialWidth = $spinner.width();
+  let divider      = images.length;
+  let sectionWidth = initialWidth/divider;
+  
+  for (var j = 0; j < images.length; j++){
+    sections[j] = {   
+      min: sectionWidth * j,
+      max: sectionWidth + (sectionWidth * j),
+      index: j
+    };
+  }
+};
 
-  // for (var i = 0; i < images.length; i++){
-  //   sections[i] = sectionWidth * i;
-  // } 
+divideContainer($listItems);
 
-  // console.log(sections);
-  // return sections; 
-}
-
-divideContainer(listItems);
-
-
-// divideContainer(listItems).then(function(sections){
-//   console.log('grabbed sections', sections)
-// })
-
-let i = 0
 $spinner.on("mousemove", function(e) {
 
   let offset = $(this).offset();
   let relX = e.pageX - offset.left;
-  console.log(relX);
-  // if( relX > sections[8] ){
-  //   //
-  // }  
-  $(".spinner-container li").each(function( index ) {
-    if (relX < i) {
-      console.log("adsf")
-      $(this).addClass("active")  
+
+  $.each(sections, function(){
+    if(relX >= this.min && relX <= this.max) {
+      $listItems.removeClass('active');
+      $listItems.eq(this.index).addClass("active");
     }
-    i += sectionWidth.to_i
-  })
-
+  });
 });
-
-//  sample code from website
-// 
-// $spinner.on("mousemove", function(e) {
-//   $(">ul>li", $spinner).each(function() {
-//     var t = e.pageX
-//       , o = $spinner.width() / 2
-//       , i = Math.abs(t - o)
-//       , n = parseFloat($(this).attr("data-speed"))
-//       , a = $("> li", this).length
-//       , s = Math.floor(i / n % a);
-//     o > t && (s = Math.abs(s - (a - 1))),
-//     $("> li.active", this).removeClass("active"),
-//     $("> li", this).eq(s).addClass("active");
-//   });
-// });
